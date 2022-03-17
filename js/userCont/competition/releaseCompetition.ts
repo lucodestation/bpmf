c3c29099784b3b652d0f49983c8e3b1660f22c6d
卢忠伟 <lucodestation@163.com>
2022-03-17 08:54:05 +0800

    封装文件上传方法，个人中心添加发布赛事页面

--- js/userCont/competition/releaseCompetition.ts
+++ js/userCont/competition/releaseCompetition.ts
@@ -4,14 +4,17 @@
 $('.public-header').load('/components/PublicHeader.html')
 // 引入底部
 $('.public-footer').load('/components/PublicFooter.html')
+// 引入侧边栏
+$('.public-user').load('/components/CenterAside.html')
 
 var encrypt = new JSEncrypt()
 //公钥.
 const publiukey =
   '-----BEGIN PUBLIC KEY-----MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCSjs8JJr/Nyb+nOG77agUDf7uTc+kswdVEXbU8v5EL98brAw7fu4dQc1vkh1KSXqiC9EC7YmJzkkFoXUzTH2pvvDlqUuCwtdmXOsq/b1JWKyEXzQlPIiwdHnAUjGbmHOEMAY3jKEy2dY2I6J+giJqo8B2HNoR+zv3KaEmPSHtooQIDAQAB-----END PUBLIC KEY-----'
 
-new Vue({
-  el: '#app',
+// 赛事公告
+const competitionAnnouncement = {
+  template: '#competitionAnnouncement',
   data() {
     return {
       // 赛事类型列表（从服务器获取）
@@ -711,5 +714,13 @@ new Vue({
         layer.msg(res.msg)
       }
     },
+  },
+}
+
+new Vue({
+  el: '#app',
+  components: {
+    // 赛事公告
+    'competition-announcement': competitionAnnouncement,
   },
 })
--- utils/util.ts
+++ utils/util.ts
@@ -12,3 +12,86 @@ util.getExtensionName = (fileName) => {
     return arr[arr.length - 1]
   }
 }
+
+/**
+ * 上传（单个）文件
+ * @param option <object>
+ * @param option.fileName <string> 文件名
+ * @param option.file <file> 要上传的文件
+ * @returns
+ */
+util.uploadFile = (option) => {
+  return new Promise(async (resolve, reject) => {
+    // 获取阿里云sts_token
+    const OSSInfo = await request({
+      url: '/api/common/sts_token',
+    })
+    console.log('OSSInfo', OSSInfo)
+
+    const client = new OSS({
+      // yourRegion填写Bucket所在地域。以华东1（杭州）为例，yourRegion填写为oss-cn-hangzhou。
+      region: 'oss-cn-beijing',
+      // 从STS服务获取的临时访问密钥（AccessKey ID和AccessKey Secret）。
+      accessKeyId: OSSInfo.data.AccessKeyId,
+      accessKeySecret: OSSInfo.data.AccessKeySecret,
+      // 从STS服务获取的安全令牌（SecurityToken）。
+      stsToken: OSSInfo.data.SecurityToken,
+      // 填写Bucket名称。
+      bucket: 'bpmf',
+    })
+
+    const fileName = new Date().valueOf() + '-' + option.fileName + '.' + util.getExtensionName(option.file.name)
+
+    const result = await client.put(fileName, option.file).catch((error) => {
+      console.log('上传失败', error)
+      reject(error)
+    })
+    console.log(result)
+    resolve(result)
+  })
+}
+
+/**
+ * 上传（多个）文件
+ * @param option <array[object]>
+ * @param option[n] <object>
+ * @param option[n].fileName <string>
+ * @param option[n].file <file>
+ * @returns
+ */
+util.uploadMultipleFile = async (option) => {
+  // 获取阿里云sts_token
+  const OSSInfo = await request({
+    url: '/api/common/sts_token',
+  })
+  console.log('OSSInfo', OSSInfo)
+
+  const client = new OSS({
+    // yourRegion填写Bucket所在地域。以华东1（杭州）为例，yourRegion填写为oss-cn-hangzhou。
+    region: 'oss-cn-beijing',
+    // 从STS服务获取的临时访问密钥（AccessKey ID和AccessKey Secret）。
+    accessKeyId: OSSInfo.data.AccessKeyId,
+    accessKeySecret: OSSInfo.data.AccessKeySecret,
+    // 从STS服务获取的安全令牌（SecurityToken）。
+    stsToken: OSSInfo.data.SecurityToken,
+    // 填写Bucket名称。
+    bucket: 'bpmf',
+  })
+
+  const arr = option.map((item) => {
+    return new Promise(async (resolve, reject) => {
+      const fileName = new Date().valueOf() + '-' + item.fileName + '.' + util.getExtensionName(item.file.name)
+
+      const result = await client.put(fileName, item.file).catch((error) => {
+        console.log('上传失败', error)
+        reject(error)
+      })
+      console.log(result)
+      resolve(result)
+    })
+  })
+
+  const result = await Premise.all(arr)
+  console.log(result)
+  return result
+}
